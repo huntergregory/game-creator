175a8defa9d9f631b646f101481d14fac8659d76
David Liu <dwl23@duke.edu>
2019-04-19 01:51:37 -0400

    Pair Programmed with Jonathan Yu fixing movement clipping and freezing due to collisions

--- src/Engine/src/ECS/CollisionHandler.java
+++ src/Engine/src/ECS/CollisionHandler.java
@@ -1,9 +1,6 @@
 package Engine.src.ECS;
 
-import Engine.src.Components.EnvironmentComponent;
-import Engine.src.Components.ImpassableComponent;
-import Engine.src.Components.MotionComponent;
-import Engine.src.Components.TagsComponent;
+import Engine.src.Components.*;
 import Engine.src.Triggers.Conditionals.Conditional;
 import Engine.src.Triggers.Conditionals.ObjectConditional;
 import Engine.src.Controller.LevelManager;
@@ -12,6 +9,8 @@ import Engine.src.Triggers.Events.ObjectEvents.ObjectEvent;
 import Engine.src.Triggers.Events.Event;
 
 import java.util.*;
+
+import static java.lang.Math.abs;
 
 /**
  * FIXME
@@ -94,6 +93,7 @@ public class CollisionHandler {
         }
     }
 
+    //FIXME Directional collisional logic
     private void checkCollision(Integer entity1, Integer entity2) {
         Pair<String>[] collisionTagPairs = findRelevantTagPairs(entity1, entity2);
         if (!myCollisionDetector.collides(entity1, entity2))
@@ -109,9 +109,48 @@ public class CollisionHandler {
                 activateEvents(entity2, entity1, responseListPair.getItem2());
             }
         }
-
+        correctClipping(entity1, entity2);
+        correctClipping(entity2, entity1);
         dealWithImpassable(entity1, entity2);
         dealWithImpassable(entity2, entity1);
+    }
+
+    //FIXME account for movement velocities in motioncomponent - save keyinputs for movement portion of collisionhandler
+    private boolean correctClipping(Integer toAdjust, Integer other) {
+        var environmentComponent = myEntityManager.getComponent(other, EnvironmentComponent.class);
+        var motionComponent = myEntityManager.getComponent(toAdjust, MotionComponent.class);
+        var adjBasic = myEntityManager.getComponent(toAdjust, BasicComponent.class);
+        var otherBasic = myEntityManager.getComponent(other, BasicComponent.class);
+        if (environmentComponent == null && motionComponent != null && adjBasic != null && otherBasic != null) {
+            Map<String, Double> shortestClipping = new HashMap<>();
+            if (myCollisionDetector.collideFromTop(toAdjust, other)) {
+                shortestClipping.put("top", otherBasic.getY() - adjBasic.getHeight() - adjBasic.getY());
+            }
+            if (myCollisionDetector.collideFromTop(other, toAdjust)) {
+                shortestClipping.put("bottom", otherBasic.getY() + otherBasic.getHeight() - adjBasic.getY());
+            }
+            if (myCollisionDetector.collideFromLeft(toAdjust, other)) {
+                shortestClipping.put("left", otherBasic.getX() - adjBasic.getWidth() - adjBasic.getX());
+            }
+            if (myCollisionDetector.collideFromLeft(other, toAdjust)) {
+                shortestClipping.put("right", otherBasic.getX() + otherBasic.getWidth() - adjBasic.getX());
+            }
+            Map.Entry<String, Double> min = null;
+            for (Map.Entry<String, Double> entry : shortestClipping.entrySet()) {
+                if (min == null || abs(min.getValue()) > abs(entry.getValue())) {
+                    min = entry;
+                }
+            }
+            if (min != null) {
+                if (min.getKey().equals("top") || min.getKey().equals("bottom")) {
+                    adjBasic.setY(adjBasic.getY() + min.getValue());
+                }
+                else if (min.getKey().equals("left") || min.getKey().equals("right")) {
+                    adjBasic.setX(adjBasic.getX() + min.getValue());
+                }
+            }
+        }
+        return false;
     }
 
     //FIXME duplicated between parts of collision handler and parts of Entity manager
@@ -121,15 +160,15 @@ public class CollisionHandler {
             var motion = myEntityManager.getComponent(mover, MotionComponent.class);
             if (motion == null)
                 return;
-            motion.setYVelocity(0);
-            /*if (myCollisionDetector.collideFromTop(mover, impassable) && motion.getYVelocity() > 0
+            //motion.setYVelocity(0);
+            if (myCollisionDetector.collideFromTop(mover, impassable) && motion.getYVelocity() > 0
                     || myCollisionDetector.collideFromTop(impassable, mover) && motion.getYVelocity() < 0) {
                 motion.setYVelocity(0);
             }
             else if (myCollisionDetector.collideFromLeft(mover, impassable) && motion.getXVelocity() > 0
                     || myCollisionDetector.collideFromLeft(impassable, mover) && motion.getXVelocity() < 0) {
                 motion.setXVelocity(0);
-            }*/
+            }
         }
     }
 
@@ -197,7 +236,7 @@ public class CollisionHandler {
                     ((ObjectEvent) event).setOther(other);
                     ((ObjectEvent) event).activate(myEntityManager);
                 }
-                    else
+                else
                     ((GameEvent) event).activate(myLevelManager);
             }
         }

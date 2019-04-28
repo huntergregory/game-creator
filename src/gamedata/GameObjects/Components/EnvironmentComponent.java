e7ad8709c16ff772fd9c107e2e135f6ec7fc0061
Hunter Gregory <hlg16@duke.edu>
2019-04-28 02:27:23 -0400

    added max velocities to environment component

--- src/gamedata/GameObjects/Components/EnvironmentComponent.java
+++ src/gamedata/GameObjects/Components/EnvironmentComponent.java
@@ -1,8 +1,15 @@
 package gamedata.GameObjects.Components;
 
 public class EnvironmentComponent extends Component{
+
+    private static final double MAX_X_VELOCITY = 20;
+    private static final double MAX_Y_VELOCITY = 20; //FIXME defaults
+
+
     private double myAccelX;
     private double myAccelY;
+    private double myMaxXVelocity = MAX_X_VELOCITY;
+    private double myMaxYVelocity = MAX_Y_VELOCITY;
     private double myVelDampener;
     private double myFriction;
 
@@ -27,5 +34,25 @@ public class EnvironmentComponent extends Component{
 
     public double getFriction() {
         return myFriction;
+    }
+
+    public double getMaxXVelocity() {
+        return myMaxXVelocity;
+    }
+
+    public double getMaxYVelocity() {
+        return myMaxYVelocity;
+    }
+
+    public void setMaxXVelocity(double max) {
+        if (max < 0)
+            max = 0;
+        myMaxXVelocity = max;
+    }
+
+    public void setMaxYVelocity(double max) {
+        if (max < 0)
+            max = 0;
+        myMaxYVelocity = max;
     }
 }

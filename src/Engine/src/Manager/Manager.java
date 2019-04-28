535f8d2d301891cc74a900738caf98e3332d6b0e
Hunter Gregory <hlg16@duke.edu>
2019-04-28 03:28:13 -0400

    copied level manager functionality into manager. some methods need to be hidden and made non public tho

--- src/Engine/src/Manager/Manager.java
+++ src/Engine/src/Manager/Manager.java
@@ -1,45 +1,74 @@
 package Engine.src.Manager;
 
-import NoEntityException;
+import Engine.src.Timers.Timer;
+import Engine.src.Timers.TimerSequence;
 import gamedata.GameObjects.Components.Component;
 import gamedata.GameObjects.Components.LivesComponent;
+import gamedata.Scene;
 import groovy.lang.Binding;
 import groovy.lang.GroovyShell;
 import groovy.lang.Script;
 
+import java.util.List;
 import java.util.Map;
 
+/**
+ * Combines the old LevelManager and EntityManager
+ */
 public class Manager {
     private Map<Integer, Map<Class<? extends Component>, Component>> myEntityMap;
+
+    private Map<Integer, Timer> myTimers;
+    private List<TimerSequence> myTimerSequences;
+    private double myCount;
 
     public Manager(Map<Integer, Map<Class<? extends Component>, Component>> entityMap, double stepTime) {
         myEntityMap = entityMap;
     }
 
-    public void call(String commandClass, String instanceID, Object ... args) {
+    public void call(String eventClass, String instanceID, Object ... args) {
+        try {
 
+        }
+        catch (
     }
 
+    public void addTimer(String eventsWhileOn, String eventsAfter, double duration) {
+        int max = 0;
+        for(int ID : myTimers.keySet()){
+            if (ID > max) max = ID;
+        }
+        myTimers.put(max + 1, new Timer(eventsWhileOn, eventsAfter, duration, myCount));
+    }
 
-    public void die(int entityID) {
-        //TODO error checking, does removing a non-existent Entity work
-        if(hasComponent(entityID, LivesComponent.class)){
-            LivesComponent lives = getComponent(entityID, LivesComponent.class);
-            if (lives.expired()) myEntityMap.remove(entityID);
+    //FIXME should not be public/open to author
+    public void updateSequences() {
+        for (TimerSequence sequence : myTimerSequences) {
+            Timer currentTimer = sequence.getCurrentTimer();
+            if (currentTimer.getCount() >= currentTimer.getEndTime()){
+                currentTimer.activateEvents(currentTimer.getMyEventsAfterTimer(), myEntityManager, this);
+                sequence.setNextTimer(myCount);
+            }
             else {
-                respawn(entityID, lives.getRespawnInstructions());
-                lives.removeLife();
+                currentTimer.activateEvents(currentTimer.getStateWhileTimerIsOn(), myEntityManager, this);
+                currentTimer.increment();
+            }
+            if (sequence.completed() && sequence.isLoop()) sequence.reset(myCount);
+            else myTimers.remove(sequence);
+        }
+    }
+
+    //FIXME should not be public/open to author
+    public void updateTimers() {
+        for (int timerID : myTimers.keySet()) {
+            Timer timer = myTimers.get(timerID);
+            if (timer.getCount() >= timer.getEndTime()){
+                timer.activateEvents(timer.getMyEventsAfterTimer(), this);
+            }
+            else {
+                timer.activateEvents(timer.getStateWhileTimerIsOn(), this);
+                timer.increment();
             }
         }
-        else myEntityMap.remove(entityID);
-    }
-
-    private void respawn(int entityID, String respawnInstructions){
-        Binding binding = new Binding();
-        binding.setProperty("ID", entityID);
-        binding.setProperty("manager", this);
-        GroovyShell shell = new GroovyShell(binding);
-        Script instructions = shell.parse(respawnInstructions);
-        instructions.run();
     }
 }

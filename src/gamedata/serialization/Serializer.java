0914454dd69518483c1ea93e89d8917804a9c052
Hunter Gregory <hlg16@duke.edu>
2019-04-29 01:42:32 -0400

    made deserialize throw an exception

--- src/gamedata/serialization/Serializer.java
+++ src/gamedata/serialization/Serializer.java
@@ -3,21 +3,15 @@ package gamedata.serialization;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
-import java.util.logging.Level;
 
 public class Serializer {
     private static final String FILE_PATH = ""; //FIXME
 
-    Gson myGson;
+    private Gson myGson;
 
     public Serializer() {
         myGson = new Gson();
@@ -28,18 +22,11 @@ public class Serializer {
         saveJSON(jsonString);
     }
 
-    public List<String> deserialize(String filePath) {
-        try {
-            String jsonString = Files.readString(Paths.get(filePath));
-            var listOfMyClassObject = new TypeToken<List<String>>() {}.getType();
+    public List<String> deserialize(String filePath) throws IOException {
+        String jsonString = Files.readString(Paths.get(filePath));
+        var listOfMyClassObject = new TypeToken<List<String>>() {}.getType();
 
-            Gson gson = new Gson();
-            return gson.fromJson(jsonString, listOfMyClassObject);
-        }
-        catch (IOException e) {
-            System.out.println("File error");
-        }
-
+        return myGson.fromJson(jsonString, listOfMyClassObject);
     }
 
     private void saveJSON(String jsonString) {

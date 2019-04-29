5d84f048f88c5177a9894550c90aafeb0ca580e2
Hunter Gregory <hlg16@duke.edu>
2019-04-29 17:33:23 -0400

    game has currentlevel as an int now

--- src/gamedata/Game.java
+++ src/gamedata/Game.java
@@ -7,21 +7,14 @@ import java.util.List;
 
 public class Game {
     public List<Scene> scenes;
-    public Scene currentScene;
     public List<GameObject> gameObjects;
     public List<Resource> resources;
+    public int currentLevel;
 
     public Game() {
         scenes = new ArrayList<>();
         gameObjects = new ArrayList<>();
         resources = new ArrayList<>();
-    }
-
-    public void addScene(Scene scene){
-        scenes.add(scene);
-    }
-
-    public void setCurrentScene(Scene scene) {
-        currentScene = scene;
+        currentLevel = 0;
     }
 }

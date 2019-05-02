b84f5cea3d84ed9a69b3dc864efc27458863ae25
Hunter Gregory <hlg16@duke.edu>
2019-05-02 02:21:06 -0400

    Revert "components werent copied when instances were created from an engine game object. Fixed that problem by making engine Game object implement cloneable"
    This reverts commit e4b1c3d861d0531956615965408dd073b4f9372e.

--- src/Engine/src/EngineData/EngineGameObject.java
+++ src/Engine/src/EngineData/EngineGameObject.java
@@ -1,21 +1,13 @@
 package Engine.src.EngineData;
 
-import Engine.src.Controller.NoInstanceException;
-import Engine.src.Controller.NoObjectException;
-
-public class EngineGameObject extends ComponentContainer implements Cloneable {
+public class EngineGameObject extends ComponentContainer {
 
     public EngineGameObject(String id) {
         super(id);
     }
 
-    public EngineInstance createInstance(String id) throws NoInstanceException {
-        try {
-            var copy = (EngineGameObject) this.clone();
-            return new EngineInstance(id, getID(), copy.getComponents());
-        }
-        catch (CloneNotSupportedException e) {
-            throw new NoObjectException(this.getID() + ". Can't clone Engine Game Object.");
-        }
+    public EngineInstance createInstance(String id) {
+        return new EngineInstance(id, getID(), getComponents());
     }
+
 }
